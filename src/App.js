8b1922237f2e853079ed5a6cff5298e41de10570
unknown <bary.berdiyev.97@mail.ru>
2022-11-05 12:07:19 +0700

    landing-page

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages";
+import SigninPage from "./pages/signin";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SigninPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,57 @@
+import svg1 from "../../images/svg-1.svg";
+import svg2 from "../../images/svg-2.svg";
+import svg3 from "../../images/svg-3.svg";
+
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to out exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: svg1,
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to out exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: svg2,
+  alt: "running",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Get access to out exclusive app that allows you to send unlimited transactions without getting charged any fees.",
+  buttonLabel: "Get started",
+  imgStart: true,
+  img: svg3,
+  alt: "Car",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
